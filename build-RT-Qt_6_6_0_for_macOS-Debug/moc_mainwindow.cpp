408046bc09422f68450c5310142d63fbef1380cb
Saharochekl <izjuha@gmail.com>
2024-12-21 20:06:01 +0300

    iiii

--- build-RT-Qt_6_6_0_for_macOS-Debug/moc_mainwindow.cpp
+++ build-RT-Qt_6_6_0_for_macOS-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../RT/mainwindow.h"
+#include "../SVB_KG/RT/mainwindow.h"
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
--- build-RT-Qt_6_6_0_for_macOS-Debug/moc_qraytracingwidget.cpp
+++ build-RT-Qt_6_6_0_for_macOS-Debug/moc_qraytracingwidget.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../RT/qraytracingwidget.h"
+#include "../SVB_KG/RT/qraytracingwidget.h"
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
