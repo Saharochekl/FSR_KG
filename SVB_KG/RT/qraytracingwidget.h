c74b39da4a48c23f9aaa76ab94d0621c3c169f4d
Saharochekl <izjuha@gmail.com>
2025-12-13 11:46:58 +0300

    tmp3

--- SVB_KG/RT/qraytracingwidget.h
+++ SVB_KG/RT/qraytracingwidget.h
@@ -16,7 +16,14 @@ public:
 
     void MakeFilm();
 
-signals:
+private slots:
+    void onTick();
+    void onFrameReady(const QImage& img);
+
+private:
+    QImage frame;
+    bool renderInProgress = false;
+    QTimer* timer = nullptr;
 
 
 
